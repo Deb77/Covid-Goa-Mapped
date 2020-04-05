a4b547667362c7c29b6e8ea82a7a6fcfe57eb9e0
chickoocoutinho <benito.coutinho@gmail.com>
2020-04-05 17:11:20 +0530

    c

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ function closeOtherInfo() {
 function updateDate(id) {
     var xhr = new XMLHttpRequest();
     xhr.open("PUT", '/', true);
-    xhr.send();
+    xhr.send(id);
     var date= new Date();
     document.getElementById("last_open").innerHTML = `last open on ${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 }
